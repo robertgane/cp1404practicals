661bc8181792078fdca797bc51a1ba55cd01fb27
Robert Gane <robert.gane@my.jcu.edu.au>
2022-09-29 23:25:57 +1000

    Refactor tempuratures program to use functions.

--- prac_02/tempuratures.py
+++ prac_02/tempuratures.py
@@ -8,14 +8,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def calculate_fahrenheit(celsius_value):
+    fahrenheit_value = celsius_value * 9.0 / 5 + 32
+    return fahrenheit_value
+
+
+def calculate_celsius(fahrenheit_value):
+    celsius_value = 5 / 9 * (fahrenheit_value - 32)
+    return celsius_value
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = calculate_fahrenheit(celsius)
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = calculate_celsius(fahrenheit)
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
