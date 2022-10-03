129347c466553547c545dbdad8fc404ea66a934e
Robert Gane <robert.gane@my.jcu.edu.au>
2022-10-03 23:54:33 +1000

    Practical 3 random numbers practice.

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+"""CP1404/CP5632 - Practical - Random Numbers"""
+import random
+
+# Line 1
+# I saw random numbers between 5 and 20(5 and 20 included) as specified in the parameters of randint
+
+# Line 2
+# I saw random numbers from 3 upto 10 counting up by 2. Smallest output number is 3 and largest is 9.
+# line 2 could not output 4 as we start from 3 and count up by 2, so the next number was 5.
+
+# Line 3
+# I found random numbers with upto 16 decimal places that ranged between 2.5 and 5.5.
+# The smallest number i could have seen is 2.5 amd the largest is 5.5
+
+print(random.randint(1, 100))
