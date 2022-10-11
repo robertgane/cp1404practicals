6a20689af7cc11264358713e7059bc1fda7f81fe
Robert Gane <robert.gane@my.jcu.edu.au>
2022-10-11 19:11:21 +1000

    Practical 4 work.

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -30,15 +30,21 @@ print(a_names)
 print(" ".join(sorted(names)))
 
 # TODO: list comprehension to create a list of all the full_names in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [full_name.lower() for full_name in full_names]
+print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
-# numbers =
+numbers = [int(number) for number in almost_numbers]
+print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+large_numbers = [number > 9 for number in numbers]
+print(large_numbers)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
+split_names = ", ".join([name.split()[1] for name in full_names if len(name) > 11])
+print(split_names)
--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,28 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    for data_point in data:
+        print(f"{data_point[0]} is taught by {data_point[1]:12} and has {data_point[2]:3} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        data.append(parts)
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
+        # line = line.strip()  # Remove the \n
+        # parts = line.split(',')  # Separate the data into its parts
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        # parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
+        # print("----------")
     input_file.close()
+    return data
 
 
 main()
