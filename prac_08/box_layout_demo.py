acb2996752f3a34b93dcba16cbf1e5a9d64b3360
Robert Gane <robert.gane@my.jcu.edu.au>
2022-11-19 11:26:31 +1000

    Complete project class program.

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.lang import Builder
+import random
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        print("test")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def clear_buttons(self):
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
+
+
+BoxLayoutDemo().run()
